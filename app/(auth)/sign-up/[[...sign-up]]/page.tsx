91b08dbb27f8a8e4ebc174822463c3e067286b6b
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-05-10 00:59:54 +0200

    Setup clerk for authentication and started navitems implementation

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignUp path='/sign-up'/>
+  )
+}
+
+export default page
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Evently</h1>
-      <Button variant="destructive" className="px-4 py-2 rounded-full ml-5">Hello World</Button>
+      Home page
     </main>
   );
 }
