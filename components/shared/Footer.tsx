6bf7b83ac42bc8fab73ae879cd57f359a7ff28d8
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-05-10 02:12:53 +0200

    Implemented footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='wrapper flex-center flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href="/">
+          <Image 
+            src="/assets/images/logo.svg"
+            alt='logo'
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>Evently. 2024 All Rights reserved</p>
+      </div>
+    </footer>
   )
 }
 
