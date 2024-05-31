efa8b5772e536b3abc4799418e23e1b8a77b9440
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-05-31 14:08:31 +0200

    Implementing Stripe

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -41,8 +41,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                 <DeleteConfirmation eventId={event._id}/>
             </div>
         )}
-        <Link 
-        href={`/events/${event._id}`} 
+        <div 
         className='flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'
         >
             <div className='flex gap-2'>
@@ -57,9 +56,12 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             <p className='p-medium-16 p-medium-18 text-grey-500'>
                 {formatDateTime(event.startDateTime).dateTime}
             </p>
-            <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>
-                {event.title}
-            </p>
+            
+            <Link href={`/events/${event._id}`}>
+                <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>
+                    {event.title}
+                </p>
+            </Link>
             <div className='flex-between w-full'>
                 <p className='p-medium-14 md:p-medium-16 text-grey-600'>
                     {event.organizer.firstName} {event.organizer.lastName}
@@ -77,7 +79,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                     </Link>
                 )}
             </div>
-        </Link>
+        </div>
     </div>
   )
 }
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { CreateEventParams, DeleteEventParams, GetAllEventsParams, GetRelatedEventsByCategoryParams, UpdateEventParams } from "@/types";
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams, GetEventsByUserParams, GetRelatedEventsByCategoryParams, UpdateEventParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import Event from "../database/models/event.model";
@@ -132,6 +132,31 @@ export const getRelatedEventsByCategory = async ({ categoryId, eventId, limit = 
             data: JSON.parse(JSON.stringify(events)),
             totalPages: Math.ceil(eventsCount / limit)
         }
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export const getEventsByUser = async ({ userId, limit = 6, page }: GetEventsByUserParams) => {
+    try {
+        await connectToDatabase();
+
+        const conditions ={ organizer: userId };
+        const skipAmount = (page - 1) * limit;
+
+        const eventsQuery = Event.find(conditions)
+            .sort({ createdAt: 'desc' })
+            .skip(skipAmount)
+            .limit(limit)
+
+        const events = await populateEvent(eventsQuery)
+        const eventsCount = await Event.countDocuments(conditions)
+
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit)
+        }
+        
     } catch (error) {
         handleError(error);
     }
